fcbdd9e89e9d5eb7e41ca35eeb1520ce0420a42a
DESKTOP-CF8BECE\dtbq1 <965804402>
2022-07-20 12:19:49 +0800

    save user information,需要再设置filter放行

--- springbootd1/src/main/java/org/example/filter/AuthoFilter.java
+++ springbootd1/src/main/java/org/example/filter/AuthoFilter.java
@@ -1,13 +1,15 @@
 package org.example.filter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+@Slf4j
 @Component
 //@WebFilter({"/test"})
 public class TestFilter implements Filter {
